537d4d0a51199ed0a4c186dc0586ef8a5e6393eb
Jason Portilla <jasonkev21@gmail.com>
2020-11-05 18:55:56 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -4,3 +4,25 @@
 // Puerto
 // ================
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ================
+// Entorno
+// ================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ================
+// Base de Datos
+// ================
+let urlDB;
+
+if( process.env.NODE_ENV  === 'dev' ) {
+  urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+  urlDB = 'mongodb+srv://JasonMaster:09cWQffK37n4Cdov@cluster0.lsjbt.mongodb.net/cafe?retryWrites=true&w=majority';
+}
+
+process.env.URLDB = urlDB;
+
