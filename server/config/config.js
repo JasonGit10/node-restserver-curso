23a9aae880691714f0effde077e60d023a655518
Jason Portilla <jasonkev21@gmail.com>
2020-11-05 19:11:39 -0300

    Variable de entorno MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ let urlDB;
 if( process.env.NODE_ENV  === 'dev' ) {
   urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-  urlDB = 'mongodb+srv://JasonMaster:09cWQffK37n4Cdov@cluster0.lsjbt.mongodb.net/cafe?retryWrites=true&w=majority';
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
